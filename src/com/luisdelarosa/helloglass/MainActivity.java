338ad6e52cfb54322bbbf1c483fd2d8813bf0383
Luis de la Rosa <luis@luisdelarosa.com>
2014-05-27 15:02:57 -0400

    Add a menu to demonstrate how to display a menu in an Immersion and handle it. This menu items says Goodbye and exits the Immersion.

--- src/com/luisdelarosa/helloglass/MainActivity.java
+++ src/com/luisdelarosa/helloglass/MainActivity.java
@@ -1,8 +1,10 @@
 package com.luisdelarosa.helloglass;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +14,36 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_goodbye) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	// Display menu when user taps on touchpad or dismisses this activity if user swipes down
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+          if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+              openOptionsMenu();
+              return true;
+          } else if (keyCode == KeyEvent.KEYCODE_BACK) {
+        	  finish();
+          }
+          return false;
     }
-    
+	
 }
